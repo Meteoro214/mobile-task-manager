901c11017708b06bfbbfc1d1263d511b4b5e863e
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-09-17 20:29:49 +0200

    Update Functions.kt
    Implemented filterTasks
    Update for empty lists on listTasks and filterTasks.
    FIxed lowercase "c" behaviour when filtering

--- mobile-task-manager/src/main/kotlin/Functions.kt
+++ mobile-task-manager/src/main/kotlin/Functions.kt
@@ -25,18 +25,32 @@ fun markTaskDone(tasks : List<Task>,id: Int){
  */
 fun listTasks(tasks : List<Task>){
     println("Printing all Tasks:")
-    for (task in tasks){
-        println(taskToString(task))
-    }
-    println("All Tasks printed")
+    if (tasks.isNotEmpty()){
+        for (task in tasks){
+            println(taskToString(task))
+        }
+        println("All Tasks printed")
+    }else println("There are no tasks to print")
 }
 
 fun filterTasks(tasks : List<Task>){
     println("Press C to filter only done tasks, press anything else for undone tasks")
-    val completed : Boolean = if("C".equals(readLine()?.trim())) true else false
+    val completed : Boolean = if("C".equals(readLine()?.trim(),ignoreCase = true)) true else false
     filterTasks(tasks,completed)
 }
 
+/**
+ * Iterates over the given list and prints tasks that fulfill the filter condition
+ */
 fun filterTasks(tasks : List<Task>,completed: Boolean){
-//Imprimir las que cumplan (lambda)
+    print("Printing all tasks that are ")
+    if(!completed) print("NOT ")
+    println("completed:")
+    val filtered = tasks.filter { it[TASK_DONE] == completed }
+    if (filtered.isNotEmpty()){
+        for (task in filtered){
+            println(taskToString(task))
+        }
+        println("All filtered tasks printed")
+    }else println("No task exists after filtering")
 }
