967a2becdd8b4143d25d8e7dc79e6696b3befa17
Meteoro214 <dfabianrl2114@gmail.com>
2025-09-22 11:32:44 +0200

    Update Extensions.kt

--- mobile-task-manager/src/main/kotlin/Extensions.kt
+++ mobile-task-manager/src/main/kotlin/Extensions.kt
@@ -1,2 +1,5 @@
-class Extensions {
-}//TODO
+import java.time.LocalDate
+
+fun LocalDate.formattedDueDate() : String {
+    return this.monthValue.toString().padStart(2) +" "+ this.getDayOfMonth().toString().padStart(2);
+}
