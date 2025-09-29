6a2f50ca48ca74d3da9e13a6637c92b978072b82
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-09-29 20:08:04 +0200

    Missed pull from main

--- app/src/main/java/gal/uvigo/mobileTaskManager/Extensions.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/Extensions.kt
@@ -1,5 +1,7 @@
 import java.time.LocalDate
 
 fun LocalDate.formattedDueDate() : String {
-    return this.monthValue.toString().padStart(2) +" "+ this.getDayOfMonth().toString().padStart(2);
+    return this.monthValue.toString().padStart(2,'0') +" "+ this.dayOfMonth.toString().padStart(2,'0')
 }
+
+fun LocalDate.isFutureDate() : Boolean = this > LocalDate.now()
--- app/src/main/java/gal/uvigo/mobileTaskManager/Task.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/Task.kt
@@ -1,21 +1,37 @@
 import java.time.LocalDate
 
-class Task(val id: Int, var title : String, var  isDone : Boolean = false,
-           var description : String? = null, var dueDate : LocalDate, var category : Category = Category.OTHER){
-//Que es modificable, que es nullable, que es private?
+class Task(val id : Int, title : String, dueDate : LocalDate,
+           var category : Category = Category.OTHER, var description : String = "",var isDone : Boolean = false) {
+
+    var title : String = title.trim()
+        set(value){
+            require(!value.trim().isEmpty()) {"title must not be empty"}
+            field = value
+        }
+    var dueDate : LocalDate = dueDate
+        set(value){
+            require(dueDate.isFutureDate()) { "due date must not be in the past" }
+            field = value
+        }
+
+    init{
+        require( id > 0) {"ID must be above 0"}
+        require(!title.trim().isEmpty()) {"title must not be empty"}
+        require(dueDate.isFutureDate()) { "due date must not be in the past" }
+    }
 
     /**
      * Returns this as a String
      */
-     override fun toString(): String {
-         val sb = StringBuilder()
-         sb.append("The task with id = ${this.id} has the following information:\n")
-         sb.append("Title = ${this.title}\n")
-         sb.append("Is it done? = ${this.isDone}\n")
-         sb.append("Due Date = ${this.dueDate.formattedDueDate()}\n")
-         sb.append("Category = ${this.category}\n")
-         sb.append("Description = ${this.description}\n")
-         return sb.toString()
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("The task with id = ${this.id} has the following information:\n")
+        sb.append("Title = ${this.title}\n")
+        sb.append("Is it done? = ${this.isDone}\n")
+        sb.append("Due Date = ${this.dueDate.formattedDueDate()}\n")
+        sb.append("Category = ${this.category}\n")
+        sb.append("Description = ${this.description}\n")
+        return sb.toString()
     }
 
     /**
@@ -23,12 +39,7 @@ class Task(val id: Int, var title : String, var  isDone : Boolean = false,
      */
     fun markDone() : Boolean {
         val wasDone = this.isDone
-        this.isDone = true;
-        return !wasDone;
+        this.isDone = true
+        return !wasDone
     }
 }
-
-
-
-
-
