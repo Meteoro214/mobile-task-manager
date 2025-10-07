738b4079822adad4d4a4875b9ef5742d94f96897
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-10-07 19:50:30 +0200

    TaskAdapter done

---
+++ app/src/main/java/gal/uvigo/mobileTaskManager/TaskAdapter.kt
@@ -0,0 +1,36 @@
+package gal.uvigo.mobileTaskManager
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import gal.uvigo.mobileTaskManager.databinding.ItemTaskBinding
+import gal.uvigo.mobileTaskManager.model.TaskCollection
+
+class TaskAdapter(val taskCollection: TaskCollection) :
+    RecyclerView.Adapter<TaskAdapter.TaskHolder>() {
+
+    class TaskHolder(val taskDataBinding: ItemTaskBinding) :
+        RecyclerView.ViewHolder(taskDataBinding.root) {
+        //Uses data binding
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TaskHolder {
+        //Creates a data bind
+        val bind = ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TaskHolder(bind)
+    }
+
+    override fun onBindViewHolder(
+        holder: TaskHolder,
+        position: Int
+    ) {
+        holder.taskDataBinding.taskData = taskCollection.getTask(position)
+    }
+
+    override fun getItemCount(): Int = taskCollection.getSize()
+
+}
