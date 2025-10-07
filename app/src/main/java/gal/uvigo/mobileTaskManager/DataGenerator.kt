a9b0309dd469109ddf464cae1541ab9c59da2f25
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-10-07 20:08:07 +0200

    Refactored MainActivity

---
+++ app/src/main/java/gal/uvigo/mobileTaskManager/DataGenerator.kt
@@ -0,0 +1,14 @@
+package gal.uvigo.mobileTaskManager
+
+import gal.uvigo.mobileTaskManager.model.Task
+import gal.uvigo.mobileTaskManager.model.TaskCollection
+import java.time.LocalDate
+
+object DataGenerator {
+
+    fun createDumbData( t : TaskCollection){
+        for (i in 1..100){
+            t.addTask(Task(i,"title"+i, dueDate = LocalDate.now(), isDone = (i%2==0)))
+        }
+    }
+}
--- app/src/main/java/gal/uvigo/mobileTaskManager/TaskController.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/TaskController.kt
@@ -43,4 +43,5 @@ class TaskController {
      */
     fun filterTasks(completed: Boolean): Iterator<Task> = tasks.filter { it.isDone == completed }
 
+    fun getCollection() : TaskCollection = this.tasks
 }
