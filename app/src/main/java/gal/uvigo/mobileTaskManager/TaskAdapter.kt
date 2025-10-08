3083675ceeabcf3f3c1ebdb61b63fbd532f012c2
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-10-08 20:11:12 +0200

    Update TaskAdapter.kt

--- app/src/main/java/gal/uvigo/mobileTaskManager/TaskAdapter.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/TaskAdapter.kt
@@ -4,15 +4,22 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import gal.uvigo.mobileTaskManager.databinding.ItemTaskBinding
+import gal.uvigo.mobileTaskManager.model.Task
 import gal.uvigo.mobileTaskManager.model.TaskCollection
+import java.time.LocalDate
 
 class TaskAdapter(val taskCollection: TaskCollection) :
     RecyclerView.Adapter<TaskAdapter.TaskHolder>() {
 
     class TaskHolder(val taskDataBinding: ItemTaskBinding) :
         RecyclerView.ViewHolder(taskDataBinding.root) {
-        //Uses data binding
-    }
+        fun bind(task: Task){
+            this.taskDataBinding.taskData = task
+            //sin la linea: Carga la imagen y el titulo y falla
+            this.taskDataBinding.executePendingBindings()
+            //con la linea : Falla inmediatamente
+        }
+        }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -27,10 +34,10 @@ class TaskAdapter(val taskCollection: TaskCollection) :
         holder: TaskHolder,
         position: Int
     ) {
-        holder.taskDataBinding.taskData = taskCollection.getTask(position)
-        //sin la linea: Carga la imagen y el titulo y falla
-        holder.taskDataBinding.executePendingBindings()
-        //con la linea : Falla inmediatamente
+        //esta pillando null la task aunque la collection tenga
+        val t2 = taskCollection.getTask(position)
+        val t =  t2 ?: Task(1,"tesy", LocalDate.now(),isDone = true)
+        holder.bind(t)
     }
 
     override fun getItemCount(): Int = taskCollection.getSize()
