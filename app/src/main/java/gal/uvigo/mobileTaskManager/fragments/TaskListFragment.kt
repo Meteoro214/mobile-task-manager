398e20034273ae02eb07f42ce1763210fd2c1d07
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-10-15 17:17:32 +0200

    Create navigation components

---
+++ app/src/main/java/gal/uvigo/mobileTaskManager/fragments/TaskListFragment.kt
@@ -0,0 +1,12 @@
+package gal.uvigo.mobileTaskManager.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import gal.uvigo.mobileTaskManager.R
+
+class TaskListFragment : Fragment() {
+
+}
