1d6cda7ab6665594116accafa0c4f66f45340133
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-10-15 18:57:40 +0200

    Moved reciclerView to fragment

--- app/src/main/java/gal/uvigo/mobileTaskManager/MainActivity.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/MainActivity.kt
@@ -3,34 +3,17 @@ package gal.uvigo.mobileTaskManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import gal.uvigo.mobileTaskManager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val controller: TaskController = TaskController()
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        loadDummyData()
-        loadLayout()
         setListeners()
     }
 
-    fun loadDummyData(){
-        DataGenerator.createDumbData(controller.getCollection())
-    }
-
-    fun loadLayout() {
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
-        binding.taskRV.layoutManager = LinearLayoutManager(this)
-        binding.taskRV.adapter = TaskAdapter(controller.getCollection())
-    }
 
     fun setListeners() {
         //No current listeners
--- app/src/main/java/gal/uvigo/mobileTaskManager/fragments/TaskFormFragment.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/fragments/TaskFormFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import gal.uvigo.mobileTaskManager.R
+import gal.uvigo.mobileTaskManager.databinding.ActivityMainBinding
 
-class TaskFormFragment : Fragment() {
+class TaskFormFragment : Fragment(R.layout.fragment_task_form) {
 
 }
