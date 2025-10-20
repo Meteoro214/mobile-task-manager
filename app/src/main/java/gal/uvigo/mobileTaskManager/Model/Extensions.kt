e63960f635b0cabd2c5908d10f432f29de9b59ac
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-10-20 23:29:05 +0200

    Update Extensions.kt

--- app/src/main/java/gal/uvigo/mobileTaskManager/Model/Extensions.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/Model/Extensions.kt
@@ -1,6 +1,7 @@
 package gal.uvigo.mobileTaskManager.model
 
 import android.icu.number.IntegerWidth
+import java.time.DateTimeException
 import java.time.LocalDate
 
 fun LocalDate.formattedDueDate(): String {
@@ -10,17 +11,29 @@ fun LocalDate.formattedDueDate(): String {
 
 
 /**
- * Assumes the created date is always in the future
+ * Assumes the created date is always in the future.
+ * Returns null if format or date is not valid
  */
-fun LocalDate.createDateFromMMDD(mmdd: String): LocalDate {
-    val currentYear = LocalDate.now().year
-    val parsed = mmdd.split(" ")
-    val day = Integer.parseInt(parsed[1])
-    val month = Integer.parseInt(parsed[0])
-    var new = LocalDate.of(currentYear, month,day)
-    //Ensures the date is in the future
-    if(!new.isFutureDate()) new = LocalDate.of(currentYear+1, month,day)
-    return new
-}
+fun LocalDate.createDateFromMMDD(mmdd: String): LocalDate? =
+    if (!Regex("(0[1-9]|1[012]) (0[1-9]|[12][0-9]|3[01])").matches(mmdd)) {
+        null
+    } else {
+        val currentYear = LocalDate.now().year
+        val parsed = mmdd.split(" ")
+        val day = Integer.parseInt(parsed[1])
+        val month = Integer.parseInt(parsed[0])
+        var toRet: LocalDate?
+        try {
+            toRet = LocalDate.of(currentYear, month, day)
+            //Ensures the date is in the future if it is calid
+            if (!(toRet?.isFutureDate() ?: true)) {
+                LocalDate.of(currentYear + 1, month, day)
+            } else toRet
+
+        } catch (e: DateTimeException) { //Invalid date null
+            null
+        }
+    }
+
 
 fun LocalDate.isFutureDate(): Boolean = this >= LocalDate.now()
