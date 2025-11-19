05de3cfa3a5bc52d60a9d48d326237b02cb30826
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-11-19 20:03:12 +0100

    item header created
    Also created bindings to:
    -Give color to task categories
    -Give color to due Dates if the task is not done and the date is aproaching (yellow), today(orange) or has passed (red)

--- app/src/main/java/gal/uvigo/mobileTaskManager/ui/tasklist/adapter/BindingAdapters.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/ui/tasklist/adapter/BindingAdapters.kt
@@ -1,9 +1,12 @@
 package gal.uvigo.mobileTaskManager.ui.tasklist.adapter
 
+import android.view.View
+import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import gal.uvigo.mobileTaskManager.R
 import gal.uvigo.mobileTaskManager.data_model.Category
+import gal.uvigo.mobileTaskManager.data_model.Task
 import gal.uvigo.mobileTaskManager.data_model.formattedDueDate
 import java.time.LocalDate
 
@@ -16,9 +19,53 @@ fun bindCategoryText(tv: TextView, category: Category?) {
         Category.URGENT -> tv.context.getString(R.string.urgent_cat)
         else -> ""
     }
+    if(tv !is AutoCompleteTextView){
+        val color =  when(category){
+            Category.OTHER -> tv.context.getColor(R.color.grassgreen)
+            Category.WORK -> tv.context.getColor(R.color.skyblue)
+            Category.PERSONAL -> tv.context.getColor(R.color.purple)
+            Category.URGENT -> tv.context.getColor(R.color.red)
+            else -> tv.context.getColor(R.color.gray)
+        }
+        tv.setTextColor(color)
+    }
+}
+
+@BindingAdapter("cat_background_color")
+fun bindCategoryColorBackground(v: View, category: Category?) {
+    val color =  when(category){
+        Category.OTHER -> v.context.getColor(R.color.grassgreen)
+        Category.WORK -> v.context.getColor(R.color.skyblue)
+        Category.PERSONAL -> v.context.getColor(R.color.purple)
+        Category.URGENT -> v.context.getColor(R.color.red)
+        else -> v.context.getColor(R.color.gray)
+    }
+    v.setBackgroundColor(color)
 }
 
 @BindingAdapter("dueDate")
 fun bindDueDateText(tv: TextView, dueDate: LocalDate?) {
     tv.text = dueDate?.formattedDueDate() ?: ""
-  }
+}
+
+@BindingAdapter("expected_date")
+fun bindDueDateText(tv: TextView, task: Task) {
+    if(!task.isDone){
+        val currentDate = LocalDate.now()
+        val color = if(task.dueDate?.isBefore(currentDate) == true) {
+            //if task is undone and already due, red
+            tv.context.getColor(R.color.red)
+        } else if(task.dueDate?.isEqual(currentDate) == true){
+            //if task is undone and due TODAY, Orange
+            tv.context.getColor(R.color.orange)
+        } else if(task.dueDate?.isAfter(currentDate.plusWeeks(1)) == false){
+            //if task is undone and due this week, yellow
+            tv.context.getColor(R.color.yellow)
+        } else {
+            //else normal color
+            tv.currentTextColor
+        }
+        tv.setTextColor(color)
+
+    }
+}
