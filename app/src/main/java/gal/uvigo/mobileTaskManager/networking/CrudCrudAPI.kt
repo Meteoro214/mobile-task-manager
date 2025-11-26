2cac1c79d9214753fd9c6ce8d4ea35af16e8418b
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-11-26 18:07:12 +0100

    Update CrudCrudAPI.kt

--- app/src/main/java/gal/uvigo/mobileTaskManager/networking/CrudCrudAPI.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/networking/CrudCrudAPI.kt
@@ -1,25 +1,21 @@
 package gal.uvigo.mobileTaskManager.networking
 
 import android.content.Context
-import androidx.lifecycle.LiveData
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import androidx.room.Update
 import gal.uvigo.mobileTaskManager.data_model.Task
 
 class CrudCrudAPI(context: Context) {
     private val service = RetrofitClient.getInstance(context).service
 
     //Used to know what tasks are stored in CrudCrud
-    val fromCCIndex : MutableMap<String, Long> = mutableMapOf()
-    val toCCIndex: MutableMap<Long, String> = mutableMapOf()
+    private val _fromCCIndex : MutableMap<String, Long> = mutableMapOf()
+    private val _toCCIndex: MutableMap<Long, String> = mutableMapOf()
 
+    val fromCCIndex : Map<String,Long>
+        get() = _fromCCIndex
+    val toCCIndex: Map<Long, String>
+        get() = _toCCIndex
 
-    suspend fun insert(task: Task): Long{
-        service.insert(task)
-    }
+    suspend fun insert(task: Task): Long = service.insert(task).id
 
     //Returns empty list if no data
     suspend fun getAll(): List<Task>{
@@ -28,8 +24,8 @@ class CrudCrudAPI(context: Context) {
         val stored = service.getAll()
         for (taskCC in stored){
             list.add(taskCC.getTask())
-            fromCCIndex[taskCC._id] = taskCC.id
-            toCCIndex[taskCC.id] = taskCC._id
+            _fromCCIndex[taskCC._id] = taskCC.id
+            _toCCIndex[taskCC.id] = taskCC._id
         }
         return list
     }
