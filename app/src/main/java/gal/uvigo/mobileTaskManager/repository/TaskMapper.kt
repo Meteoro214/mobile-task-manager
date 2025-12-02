8c828479fac7375a0168d0c025269ec195a4d49c
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-12-03 00:16:36 +0100

    Update TaskMapper.kt

--- app/src/main/java/gal/uvigo/mobileTaskManager/repository/TaskMapper.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/repository/TaskMapper.kt
@@ -5,23 +5,21 @@ package gal.uvigo.mobileTaskManager.repository
  */
 class TaskMapper {
 
-    // Task to Entity (add/update)
-    // Task to SendDTO
-    // Task to GetDTO
+    // Task to Entity (add/update to Room)  _id to null, Sync to pending_insert/update, position to next one
+    // Task to SendDTO (NEVER)
+    // Task to GetDTO (NEVER)
 
+    // Entity to Task (transforming getAll and get) (ordering is done on dao, ignore position,_id, syncStatus)
+    // Entity to SendDTO (sync to CruCrud) (ignore _id, sync)
+    // Entity to GetDTO (NEVER)
 
+    // SendDTO to Task (NEVER)
+    // SendDTO to Entity (NEVER)
+    // SendDTO to GetDTO (NEVER) CrudCrud does it kinda
 
-    // Entity to Task (transforming getAll and get)
-    // Entity to SendDTO
-    // Entity to GetDTO
-
-    // SendDTO to Task
-    // SendDTO to Entity
-    // SendDTO to GetDTO
-
-    // GetDTO to Task
-    // GetDTO to Entity
-    // GetDTO to SendDTO
+    // GetDTO to Task (maybe on first launch, maybe NEVER if sync goes to Room first)
+    // GetDTO to Entity (sync on first launch, CrudCrud to Room)
+    // GetDTO to SendDTO (NEVER)
 
 
 }
