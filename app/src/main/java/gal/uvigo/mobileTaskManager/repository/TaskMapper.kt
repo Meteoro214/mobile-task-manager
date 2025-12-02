86e6c2903fc2b42e9ea889de1fa0e28348e2b862
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-12-03 00:11:13 +0100

    Create TaskMapper.kt

---
+++ app/src/main/java/gal/uvigo/mobileTaskManager/repository/TaskMapper.kt
@@ -0,0 +1,27 @@
+package gal.uvigo.mobileTaskManager.repository
+
+/**
+ * Class to handle conversions between the different classes that represent Tasks
+ */
+class TaskMapper {
+
+    // Task to Entity (add/update)
+    // Task to SendDTO
+    // Task to GetDTO
+
+
+
+    // Entity to Task (transforming getAll and get)
+    // Entity to SendDTO
+    // Entity to GetDTO
+
+    // SendDTO to Task
+    // SendDTO to Entity
+    // SendDTO to GetDTO
+
+    // GetDTO to Task
+    // GetDTO to Entity
+    // GetDTO to SendDTO
+
+
+}
