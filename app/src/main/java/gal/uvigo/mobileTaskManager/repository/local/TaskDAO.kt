5a2df3cb5e4a8b57aa436b647947f589b864812d
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-12-03 22:59:18 +0100

    Update TaskDAO.kt
    Integrated sync updates on more methods, added get(id) for sync operations, turned update to a normal query (because needing to read position and _id to update was more costly) and turned delete to a query (to delete from id only)

--- app/src/main/java/gal/uvigo/mobileTaskManager/repository/local/TaskDAO.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/repository/local/TaskDAO.kt
@@ -2,12 +2,11 @@ package gal.uvigo.mobileTaskManager.repository.local
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
-import gal.uvigo.mobileTaskManager.repository.sync.SyncStatus
+import gal.uvigo.mobileTaskManager.model.Category
+import java.time.LocalDate
 
 @Dao
 interface TaskDAO {
@@ -22,34 +21,50 @@ interface TaskDAO {
     /**
      * Expected to return number of updated rows (1).
      * Stores the CrudCrud generated _id.
-     * Call only after sync.
+     * Perform only after insert sync operations.
      */
-    @Query("UPDATE tasks SET _id = :idCC WHERE id = :id")
-    suspend fun syncInsert(id : Long, idCC : String): Int
+    @Query("UPDATE tasks SET _id = :idCC, sync_status = 'SYNCED' WHERE id = :id")
+    suspend fun syncInsert(id: Long, idCC: String): Int
+
+    /**
+     * Expected to return a single instance of TaskEntity or Null if no results.
+     */
+    @Query("SELECT * FROM tasks WHERE id = :id")
+    fun get(id: Long): TaskEntity?
 
     /**
      * Expected to return number of updated rows (1).
      */
-    @Update
-    suspend fun update(updated: TaskEntity): Int
+    @Query(
+        "UPDATE tasks SET id = :id,title=:title," +
+                "due_date=:dueDate, category = :category," +
+                " description=:description, isDone = :isDone," +
+                "sync_status = 'PENDING_UPDATE' WHERE id = :id"
+    )
+    suspend fun update(
+        id: Long, title: String,
+        dueDate: LocalDate, category: Category,
+        description: String, isDone: Boolean
+    ): Int
 
     /**
      * Expected to return number of updated rows (1).
      */
-    @Query("UPDATE tasks SET isDone = true WHERE id = :id")
+    @Query("UPDATE tasks SET isDone = true, sync_status = 'PENDING_UPDATE' WHERE id = :id")
     suspend fun markDone(id: Long): Int
 
     /**
      * Expected to return number of updated rows (1).
      */
-    @Query("UPDATE tasks SET position = :newPos WHERE id = :id")
-    suspend fun changePosition(id: Long, newPos : Int): Int
+    @Query("UPDATE tasks SET position = :newPos, sync_status = 'PENDING_UPDATE' WHERE id = :id")
+    suspend fun changePosition(id: Long, newPos: Int): Int
 
     /**
      * Expected to return number of updated rows (1).
+     * Perform only after update sync operations.
      */
-    @Query("UPDATE tasks SET sync_status = :sync WHERE id = :id")
-    suspend fun updateSyncStatus(id: Long, sync: SyncStatus = SyncStatus.SYNCED): Int
+    @Query("UPDATE tasks SET sync_status = 'SYNCED' WHERE id = :id")
+    suspend fun markSynced(id: Long): Int
 
     /**
      * Expected to return number of updated rows (1).
@@ -61,16 +76,19 @@ interface TaskDAO {
     /**
      * Expected to return number of updated rows (1).
      * This is a hard delete.
-     * Perform only after sync operations.
+     * Perform only after delete sync operations.
      */
-    @Delete
-    suspend fun trueDelete(task: TaskEntity): Int
+    @Query("DELETE FROM tasks WHERE id = :id")
+    suspend fun trueDelete(id: Long): Int
 
     /**
      * Expected to return LiveData of empty list if no values.
      * Will return tasks ordered by category and position
      */
-    @Query("SELECT * FROM tasks WHERE sync_status <> 'PENDING_DELETE' ORDER BY category,position ASC")
+    @Query(
+        "SELECT * FROM tasks WHERE sync_status <> 'PENDING_DELETE' " +
+                "ORDER BY category,position ASC"
+    )
     fun getAll(): LiveData<List<TaskEntity>>
 
 }
