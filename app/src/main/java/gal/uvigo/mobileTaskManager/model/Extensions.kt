826cbc9ffdd64e5886efe23c8cbb78e561ae11d1
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-12-20 17:05:14 +0100

    Update Extensions.kt
    Refactored methods for date formatting.
    Method to parse the date as string could be simplified

--- app/src/main/java/gal/uvigo/mobileTaskManager/model/Extensions.kt
+++ app/src/main/java/gal/uvigo/mobileTaskManager/model/Extensions.kt
@@ -7,41 +7,36 @@ import java.time.LocalDate
  * Extension functions to handle dueDate formats.
  */
 
-//TODO adapt to date picker, mantener seguro para los adapters
 
 /**
- * For this date, returns a String of the date formatted as MM DD.
+ * For this date, returns a String of the date formatted.
+ * If year is true, format is MM DD YYYY, else, it is MM DD.
  */
-fun LocalDate.formattedDueDate(): String =
-    this.monthValue.toString().padStart(2, '0') + " " + this.dayOfMonth.toString()
-        .padStart(2, '0')
-
-/**
- * For this date, returns a String of the date formatted as MM DD YYYY.
- */
-fun LocalDate.formattedDueDateWithYear(): String =
+fun LocalDate.formattedDueDate(year: Boolean = true): String =
     this.monthValue.toString().padStart(2, '0') + " " +
-            this.dayOfMonth.toString().padStart(2, '0') + " " +
-            this.year.toString().padStart(4, '0')
-
+            this.dayOfMonth.toString().padStart(2, '0') +
+            if (year) {
+                " " + this.year.toString().padStart(4, '0')
+            } else ""
 
 /**
- * Returns a date created from the given mmdd formatted date string.
- * If autoYear is set to true, will expect MM DD and add a year so the date is not in the past.
- * If autoYear is set to false, will expect MM DD YYYY and parse accordingly.
+ * Returns a date created from the given formatted date string.
+ * If year is set to false, will expect MM DD and add a year so the date is not in the past.
+ * If year is set to true, will expect MM DD YYYY and parse accordingly.
  * Returns null if format or date is not valid.
  * Will force a leap year for 29th february.
  */
-fun LocalDate.createDateFromMMDD(mmdd: String, autoYear: Boolean = true): LocalDate? =
-    if ((autoYear && !Regex("(0[1-9]|1[012]) (0[1-9]|[12][0-9]|3[01])").matches(mmdd))
-        || (!autoYear && !Regex("(0[1-9]|1[012]) (0[1-9]|[12][0-9]|3[01]) \\d{4}").matches(mmdd))
+fun LocalDate.createDateFromMMDD(formattedString: String, year: Boolean = true): LocalDate? =
+    if ((!year && !Regex("(0[1-9]|1[012]) (0[1-9]|[12][0-9]|3[01])").matches(formattedString))
+        || (year && !Regex("(0[1-9]|1[012]) (0[1-9]|[12][0-9]|3[01]) \\d{4}").matches(formattedString))
     ) {
         null
     } else {
-        val parsed = mmdd.split(" ")
+        //Could be simplified, no longer used without year
+        val parsed = formattedString.split(" ")
         val day = Integer.parseInt(parsed[1])
         val month = Integer.parseInt(parsed[0])
-        val currentYear = if (autoYear) LocalDate.now().year else Integer.parseInt(parsed[2])
+        val currentYear = if (!year) LocalDate.now().year else Integer.parseInt(parsed[2])
         try {
             val toRet = if (day == 29 && month == 2) { //need to be a leap year
                 val nextLeapYear =
@@ -58,10 +53,10 @@ fun LocalDate.createDateFromMMDD(mmdd: String, autoYear: Boolean = true): LocalD
                 LocalDate.of(currentYear, month, day)
             }
             //If needed, ensures the date is in the future if it is valid, can only be in the past if not leap
-            if (autoYear && toRet?.isBefore(LocalDate.now()) == true) {
+            if (!year && toRet?.isBefore(LocalDate.now()) == true) {
                 LocalDate.of(currentYear + 1, month, day)
             } else toRet
-        } catch (e: DateTimeException) { //Invalid date null
+        } catch (_: DateTimeException) { //Invalid date null
             null
         }
     }
